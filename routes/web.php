7c827add2285c7aa208b483a53453c3e4b19e5d0
prospero <g0lden_vo1d@protonmail.com>
2023-05-16 13:26:32 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
 use App\DataTables\UserDataTable;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (UserDataTable $dataTable) {
     return $dataTable->render('index');
 });
-
-// Auth::routes();
-
-// Route::get('/home', [HomeController::class, 'index'])->name('home');
-// Route::get('/home/get-users', [HomeController::class, 'getUsers'])->name('home.get.users');
